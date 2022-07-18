54afa917a000a5a8a984c3c89e1da7648c6dfd37
swardhaSawant <swardhasawant890@gmail.com>
2022-07-18 12:46:18 +0530

    final commit

--- stop.js
+++ stop.js
@@ -5,26 +5,26 @@ const start = document.querySelector("#start-btn");
 let [millisec, min, hr, sec]=[0,0,0,0];
 let count=false;
 
-
+// logic for start button
 start.addEventListener('click', ()=>{
+    // if watch is in active state no more calls will be considered for start function
     if(count!=false){
         clearInterval(count);
     }
     count=setInterval(display,10);
 })
-
+// logic for stop button
 stop.addEventListener('click', ()=>{
     clearInterval(count);
 })
-
+// logic for reset button
 reset.addEventListener('click', ()=>{
     clearInterval(count);
     [millisec, min, hr, sec]=[0,0,0,0];
     para.innerHTML =  "00 : 00 : 00 : 000";
 
 })
-
-
+// initializing clock to 10 miiliseconds
 function display(){
     millisec+=10;
     if(millisec==1000){
@@ -46,11 +46,6 @@ function display(){
     let s = sec < 10 ? "0" + sec : sec;
     let ms = millisec < 10 ? "00" + millisec : millisec <100 ? "0" + millisec : millisec;
 
-    
-
     para.innerHTML=`${h} : ${m} : ${s}: ${ms}`;
-    
-
-
     
 }
